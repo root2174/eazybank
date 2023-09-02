a2eac23fc18fe6b45def49a6b030d6ff17543807
Lucas Magalhães <lm031719@gmail.com>
2023-09-01 23:42:53 -0300

    Adding no op password encoder

--- src/main/java/com/eazybank/eazybankbackend/config/SecurityConfig.java
+++ src/main/java/com/eazybank/eazybankbackend/config/SecurityConfig.java
@@ -6,6 +6,8 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -33,18 +35,26 @@ public class SecurityConfig {
 
     @Bean
     public InMemoryUserDetailsManager userDetailsService() {
-        UserDetails admin = User.withDefaultPasswordEncoder()
-                .username("admin")
+        UserDetails admin = User.withUsername("admin")
                 .password("123456")
                 .authorities("admin")
                 .build();
 
-        UserDetails user = User.withDefaultPasswordEncoder()
-                .username("user")
+        UserDetails user = User.withUsername("user")
                 .password("123456")
                 .authorities("read")
                 .build();
 
         return new InMemoryUserDetailsManager(admin, user);
+    }
+
+    /**
+     * NoOpPasswordEncoder is not recommended for production.
+     *
+     * @return PasswordEncoder
+     */
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
 }
